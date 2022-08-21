5ee473d35b05eae8a259e339da095b045bac7db3
Chris McComb <ccmcc2012@gmail.com>
2022-08-21 17:28:08 -0400

    Better output

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -22,16 +22,18 @@ use rhai_rand::RandomPackage;
 use rhai_sci::SciPackage;
 
 #[tauri::command]
-fn rhai(name: &str) -> String {
+fn rhai(name: &str, window: tauri::Window) {
     let mut engine = rhai::Engine::new();
     engine.register_global_module(SciPackage::new().as_shared_module());
     engine.register_global_module(RandomPackage::new().as_shared_module());
+    let w = window.clone();
+    engine.on_print(move |x| {
+        w.eval(&format!("append_output('{}')", x));
+    });
     let script_ast = engine.compile(&name).map_err(|e| e.to_string()).unwrap();
 
-    let result: rhai::Dynamic = engine
-        .eval_ast(&script_ast)
-        .map_err(|e| e.to_string())
-        .unwrap();
-
-    format!("{:?}", result)
+    match engine.eval_ast::<rhai::Dynamic>(&script_ast) {
+        Ok(result) => window.eval(&format!("append_output('{}')", result.to_string())),
+        Err(e) => window.eval(&format!("append_output('{:?}')", e)),
+    };
 }
