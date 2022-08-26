96f72b373fdac2beb3a847e7840aa3849356e315
Chris McComb <ccmcc2012@gmail.com>
2022-08-26 09:57:40 -0400

    New icon set

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,17 +2,19 @@
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
+use std::sync::Mutex;
 use tauri::Manager;
 
-// the payload type must implement `Serialize` and `Clone`.
-#[derive(Clone, serde::Serialize)]
-struct Payload {
-    message: String,
-}
+struct MyState(Mutex<rhai::Engine>);
 
 fn main() {
+    let mut engine = rhai::Engine::new();
+    engine.register_global_module(SciPackage::new().as_shared_module());
+    engine.register_global_module(RandomPackage::new().as_shared_module());
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![rhai])
+        .manage(MyState(Mutex::new(engine)))
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
@@ -28,7 +30,22 @@ fn rhai(name: &str, window: tauri::Window) {
     engine.register_global_module(RandomPackage::new().as_shared_module());
     let w = window.clone();
     engine.on_print(move |x| {
-        w.eval(&format!("append_output('{}')", x));
+        w.eval(&format!("append_output('{}')", x.to_string()));
+    });
+    let script_ast = engine.compile(&name).map_err(|e| e.to_string()).unwrap();
+
+    match engine.eval_ast::<rhai::Dynamic>(&script_ast) {
+        Ok(result) => window.eval(&format!("append_output('{}')", result.to_string())),
+        Err(e) => window.eval(&format!("append_output('{:?}')", e)),
+    };
+}
+
+#[tauri::command]
+fn rhai_repl(name: &str, window: tauri::Window, state: tauri::State<MyState>) {
+    let mut engine = state.0.lock().unwrap();
+    let w = window.clone();
+    engine.on_print(move |x| {
+        w.eval(&format!("append_output('{}')", x.to_string()));
     });
     let script_ast = engine.compile(&name).map_err(|e| e.to_string()).unwrap();
 
